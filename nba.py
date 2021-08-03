2c4767eee1657f9497b36087158a2661f5c92368
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-03 14:54:59 -0400

    Update nba.py

--- nba.py
+++ nba.py
@@ -1,4 +1,6 @@
 import json
+import ast
+from pprint import pprint
 import re
 import statistics
 import requests
@@ -8,23 +10,27 @@ from dotenv import dotenv_values
 from dotenv.main import load_dotenv
 load_dotenv()
 
-api = os.getenv("api_key")
-#url = 'https://api.sportradar.us/nba/{access_level}/{version}/{language_code}/league/free_agents.{format}?api_key={your_api_key}'
-#final_url = f'{url}'
+API = os.getenv("api_key")
 
 print("-------------------------")
 x = input("Do you want to find the best player for your team? (Type Yes or No):")
 
-#url = "https://api.sportradar.us/nba/trial/v7/en/league/free_agents.json?"
-#final_url = f'{url}api_key={api}'
-
 conn = http.client.HTTPSConnection("api.sportradar.us")
 
-conn.request("GET", "/nba/trial/v7/en/league/free_agents.json?api_key=zcbqec5qr9v7w7s59rcfmvqm")
+conn.request("GET", f"/nba/trial/v7/en/league/free_agents.json?api_key={API}")
 
 res = conn.getresponse()
 data = res.read()
 
-print(data)
-
+#pprint(data)
+#print(type(data))
+#print(data.keys())
+# source: https://stackoverflow.com/questions/49184578/how-to-convert-bytes-type-to-dictionary
+dict_str = data.decode("UTF-8")
+mydata = ast.literal_eval(dict_str)
+#print(repr(mydata))
+print(type(mydata))
+print(mydata.keys())
+pprint(mydata)
 print("Now, what position are you looking for? (Input PG, SG, SF, PF, or C):")
+
