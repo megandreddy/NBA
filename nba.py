47b4c470b5b168a5475f85297c85d88373dc1b71
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-03 16:21:24 -0400

    Update nba.py

--- nba.py
+++ nba.py
@@ -15,9 +15,22 @@ API = os.getenv("api_key")
 print("Do you have what it takes to be a GM?")
 print("-------------------------")
 x = input("Do you want to find the best player for your team? (Type Yes or No): ")
-    if x.upper() == "NO": #source - .upper https://github.com/s2t2/shopping-cart-with-email-receipts/blob/master/checkout.py
-        break
-
+if x.upper() == "NO": #source - .upper https://github.com/s2t2/shopping-cart-with-email-receipts/blob/master/checkout.py
+    exit()
+else:
+    conn = http.client.HTTPSConnection("api.sportradar.us")
+    conn.request("GET", f"/nba/trial/v7/en/league/free_agents.json?api_key={API}")
+    res = conn.getresponse()
+    data = res.read()
+    dict_str = data.decode("UTF-8")
+    mydata = ast.literal_eval(dict_str)
+    free_agents = mydata["free_agents"]
+    #pprint(free_agents)
+    for player in free_agents:
+        print(player["full_name"], player["position"], player["height"], player["weight"], player["birthdate"])
+        #print(player["position"])
+        #for player in mydata:
+    #    print(player["full_name"], player["primary_position"])
 #while True:
 #    x = input("Do you want to find the best player for your team? (Type Yes or No): ")
 #    if x.upper() == "NO": #source - .upper https://github.com/s2t2/shopping-cart-with-email-receipts/blob/master/checkout.py
@@ -44,5 +57,4 @@ x = input("Do you want to find the best player for your team? (Type Yes or No): 
 #            print("Now, what position are you looking for? (Input PG, SG, SF, PF, or C):")
 #        except IndexError as e:
 #            print("Goodbye!")
-#        #source: https://github.com/larrydor/shopping_cart 
-#
+#        #source: https://github.com/larrydor/shopping_cart
