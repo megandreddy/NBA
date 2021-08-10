467d50de38bbd7311b96bda91faa98f3eb8b2767
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-09 21:28:57 -0400

    Update test_nba_search.py

--- test_nba_search.py
+++ test_nba_search.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 import json
 import os
+import ast
 from dotenv.main import load_dotenv
 import http.client
 load_dotenv()
@@ -13,5 +14,9 @@ def parsed_response():
     print("MAKING A NETWORK REQUEST...")    
     conn = http.client.HTTPSConnection("api.sportradar.us")
     conn.request("GET", f"/nba/trial/v7/en/teams/583ecae2-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key={API}")  
-    response = conn.getresponse()
-    return json.loads(response.text)
+    res_nba_teams = conn.getresponse()
+    nba_teams_data = res_nba_teams.read()
+    nba_teams_dict_str = nba_teams_data.decode("UTF-8")
+    nba_teams_datas = ast.literal_eval(nba_teams_dict_str)
+    team_players = nba_teams_datas["players"]
+    return json.loads(team_players)
