d4d51bcb5dde02fc4692a2e37a815188612a2d3c
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-09 21:14:39 -0400

    updates

--- test_nba_search.py
+++ test_nba_search.py
@@ -13,5 +13,6 @@ def parsed_response():
     print("MAKING A NETWORK REQUEST...")    
     conn = http.client.HTTPSConnection("api.sportradar.us")
     conn.request("GET", f"/nba/trial/v7/en/teams/583ecae2-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key={API}")
-    response = requests.get(f"/nba/trial/v7/en/teams/583ecae2-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key={API}")
+    #response = requests.get(f"/nba/trial/v7/en/teams/583ecae2-fb46-11e1-82cb-f4ce4684ea4c/profile.json?api_key={API}")
+    response = requests.get(conn.request)
     return json.loads(response.text)
