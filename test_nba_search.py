e0fa8bf834dba840ae496b2d73801265ff60f243
larrydor <86853201+larrydor@users.noreply.github.com>
2021-08-09 21:32:54 -0400

    Update test_nba_search.py

--- test_nba_search.py
+++ test_nba_search.py
@@ -19,4 +19,4 @@ def parsed_response():
     nba_teams_dict_str = nba_teams_data.decode("UTF-8")
     nba_teams_datas = ast.literal_eval(nba_teams_dict_str)
     #print(nba_teams_datas)
-    return json.loads(nba_teams_datas.text)
+    return json.loads(nba_teams_datas)
